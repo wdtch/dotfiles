16bfa5057b282e878335ee7aa2e4d09a2b538d62
wdtch <rever.psif@live.jp>
2016-04-18 16:58:29 +0900

    modified folder name to clone

--- install.sh
+++ install.sh
@@ -1,12 +1,12 @@
-git clone https://github.com/wdtch/dotfiles
-ln -s ./dotfiles/.zshrc ~/.zshrc
+git clone --recursive https://github.com/wdtch/dotfiles .dotfiles
+ln -s ~/.dotfiles/.zshrc ~/.zshrc
 
 if [ "$(uname)" == 'Darwin' ]; then
   OS='Mac'
-  ln -s ./dotfiles/Sublime\ Text\ 3 ~/Library/Application\ Support/Sublime\ Text\ 3
+  ln -s ./.dotfiles/Sublime\ Text\ 3 ~/Library/Application\ Support/Sublime\ Text\ 3
 elif [ "$(expr substr $(uname -s) 1 5)" == 'Linux' ]; then
   OS='Linux'
-  ln -s ./dotfiles/Sublime\ Text\ 3 ~/.config/sublime-text-3
+  ln -s ./.dotfiles/Sublime\ Text\ 3 ~/.config/sublime-text-3
 # elif [ "$(expr substr $(uname -s) 1 10)" == 'MINGW32_NT' ]; then
   # OS='Cygwin'
 else
